f721f453848ef57f4140cccd4d3a97cfbb0c9bc1
LTX74 <aaronpernet@gmail.com>
2026-01-11 01:23:35 +0100

    Add files via upload

---
+++ Auto-K6/Auto-K6-V2/setup.bat
@@ -0,0 +1,20 @@
+@echo off
+title Auto-K6-V2 Installer
+color 1
+echo Verifying Python installation...
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed. Please install Python and try again.
+    pause
+    exit /b 1
+)
+cls
+echo.
+echo Installing required packages...
+echo.
+pip install -r requirements.txt
+cls
+echo.
+echo Installation complete press any key to start Auto-K6-V2.py !
+pause>nul
+python Auto-K6-V2.py
